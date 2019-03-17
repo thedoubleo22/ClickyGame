7aaa44e35c190b09036bc372587d65312adb0fc7
Olivia Oxley <ooxley918@gmail.com>
2019-03-17 11:56:36 -0700

    styling updates

---
+++ build/precache-manifest.6016c2726bfefdf9c6e30d91290d54fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b41e4230d8e36ca9e04",
+    "url": "/clickygame/static/css/main.714e6f86.chunk.css"
+  },
+  {
+    "revision": "4b41e4230d8e36ca9e04",
+    "url": "/clickygame/static/js/main.a979e3a6.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "88a1b0efb6185fd25610",
+    "url": "/clickygame/static/js/2.e2cb34fa.chunk.js"
+  },
+  {
+    "revision": "9905bf1367d276e0debc2f9cc738636f",
+    "url": "/clickygame/index.html"
+  }
+];
